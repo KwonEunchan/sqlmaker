225f4c17aed651ad53d7b09b6dc8c56d9bf1d15b
kwon <klg3377@naver.com>
2022-12-30 14:00:52 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,92 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 import info from './info.json'
 
+function makeSelect(ptr){
+  let copy = ''
+  console.log(ptr);
+  copy = 'SELECT\n'
+  info.info[ptr].column.forEach((columnName,index)=>{
+    copy +=  index===0 ? '\t  ' : '\t, ' 
+    copy += columnName
+    copy += '\n'
+  })
+  copy += '\t, SYSDATE AS INPUT_DT\n'
+  copy += '\t, \'Y\' AS IF_YN\n'
+  copy += '\t, IF_DT\n'
+  copy += '\t, \'정상이관\' AS IF_RES\n'
+  copy += '\nFROM\n\t'
+  copy += info.info[ptr].table
+  copy += '\n\nWHERE 1=1\n\tAND '
+  copy += info.info[ptr].condition
+  
+  return copy
+}
+
+function makeInsert(ptr){
+  let copy = ''
+  copy += `INSERT INTO ${info.info[ptr].table}\n(\n`
+  info.info[ptr].column.forEach((columnName,index)=>{
+    copy +=  index===0 ? '\t  ' : '\t, ' 
+    copy += columnName
+    copy += '\n'
+  })
+  copy += ')\n\nVALUES\n('
+  info.info[ptr].column.forEach((columnName,index)=>{
+    copy +=  index===0 ? '\t  ' : '\t, ' 
+    copy += `:/MSG_DB_COLUMN_ARR_0.LIST*.${columnName}\n`
+  })
+  copy += '\n)'
+  
+  return copy
+}
+
+function makeUpdate(ptr){
+  let copy = ''
+  copy = `UPDATE ${info.info[ptr].table}\n`
+  copy += '\nSET\n'
+  copy += '\tIF_YN = :/MSG_DB_COLUMN_ARR_0.LIST*.IF_YN\n'
+  copy += '\t,IF_DT = SYSDATE\n'
+  copy += '\t,IF_RES = :/MSG_DB_COLUMN_ARR_0.LIST*.IF_RES\n'
+  copy += '\t,IF_GUID = :/MSG_COMMON_HEADER_1.IF_GUID\n'
+  copy += '\nWHERE 1=1\n'
+
+  info.info[ptr].pk.forEach((pk_index,index)=>{
+    copy +=  '\tAND '
+    copy += `${info.info[ptr].column[pk_index]}=`
+    copy += `:/MSG_DB_COLUMN_ARR_0.LIST*.${info.info[ptr].column[pk_index]}\n`
+})
+  return copy
+}
+
 function App() {
+  const [ptr,setPtr] = useState(0)
+  const [sql,setSql] = useState('')
+
+  useEffect(()=>{
+    document.querySelectorAll('.menuEl').forEach((el,index)=>{
+      if(index===ptr) el.classList.add('active')
+      else el.classList.remove('active')
+    })
+  },[ptr])
+
   return (
     <div className="App">
+      {
+        
+        <div className="aboutPage" onClick={(e)=>{
+          e.target.classList.toggle('active')
+        }}>
+        <div className="aboutBox" onClick={(e)=>{
+          e.stopPropagation()
+        }}>
+          <div className="inner">
+            <p>{info.info[ptr].name} - {sql.split(' ',1)} </p>
+            <textarea className="target" disabled></textarea>
+          </div>
+        </div>
+      </div>
+      }
       {
         <div className="inner">
             <div className="menuList">
@@ -11,27 +94,56 @@ function App() {
             info.info.map((el,index)=>{
               return(
                 <div className="menuEl" key={index} onClick={(e)=>{
-                  document.querySelectorAll('.menuEl').forEach((el)=>{
-                    el.classList.remove('active')
-                  })
-                  e.target.classList.add('active')                                    
+                    setPtr(index)
                 }}>{el.name}</div>
               )
             })
           }
           </div>
           <div className="contentBox">
+            <div className="inner">
+            <div className="intro">
+              {
+                ptr<0 ?
+                <p>왼쪽에서 거래를 선택해주세요 !</p>
+                  :
+                  <>
+                  <p>왼쪽에서 거래를 선택해주세요 !</p>
+                <p>아래에서 원하는 메뉴를 선택해주세요 !</p>
+                </>
+              }
+            </div>
             <div className="sqlList">
               {
-                [1,2,3].map((el,index)=>{
+                ['SELECT','INSERT','UPDATE'].map((el,index)=>{
                   return(
-                  <div className="sqlEl">
-                    <p className="number">0{el}.</p>
-                    <p className='name'>SELECT</p>
+                  <div className="sqlEl"  key={index}>
+                    <p className="number">0{index+1}.</p>
+                    <p className='name'>{el}</p>
+                    {
+                      ptr>=0 && <p className="info">{info.info[ptr].name} - {el}</p>
+                    }
+                    {
+                      ptr>=0 && <div className="btnList">
+                      <button className='copy' onClick={(e)=>{
+                        let copy = index === 0 ? makeSelect(ptr) : index===1 ? makeInsert(ptr) : makeUpdate(ptr)
+                        document.querySelector('.target').value = copy
+                        console.log(document.querySelector('.target'))
+                        document.querySelector('.target').select()
+                        document.execCommand('copy')
+                      }}>복 사</button>
+                      <button className='about' onClick={(e)=>{
+                        document.querySelector('.aboutPage').classList.toggle('active')
+                        let copy = index === 0 ? makeSelect(ptr) : index===1 ? makeInsert(ptr) : makeUpdate(ptr)
+                        document.querySelector('.target').value = copy
+                      }}>원문 보기</button>
+                    </div>
+                    }
                   </div>
                   )
                 })
               }
+          </div>
           </div>
           </div>
         </div>
